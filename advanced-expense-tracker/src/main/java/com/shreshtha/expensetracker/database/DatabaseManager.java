11eb752da83332efc3653da514cfeee479efa5e9
shreshthabagohil <shreshthabagohil@gmail.com>
2026-03-01 22:26:43 +0530

    Modify database schema for expenses and budgets
    Updated the expenses and budgets tables in the database schema to include username and mood fields.

--- advanced-expense-tracker/src/main/java/com/shreshtha/expensetracker/database/DatabaseManager.java
+++ advanced-expense-tracker/src/main/java/com/shreshtha/expensetracker/database/DatabaseManager.java
@@ -1,55 +1,46 @@
 package com.shreshtha.expensetracker.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
 
-    private static final String URL = "jdbc:sqlite:expense_tracker.db";
+    private static final String URL =
+            "jdbc:sqlite:expense_tracker.db";
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL);
     }
 
     public static void initializeDatabase() {
+
         try (Connection conn = connect();
              Statement stmt = conn.createStatement()) {
-
-                stmt.execute("""
-    CREATE TABLE IF NOT EXISTS expenses (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user TEXT,
-        category TEXT,
-        amount REAL,
-        date TEXT,
-        description TEXT
-    )
-""");
 
             String createExpensesTable = """
                 CREATE TABLE IF NOT EXISTS expenses (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT,
                     category TEXT NOT NULL,
                     amount REAL NOT NULL,
                     date TEXT NOT NULL,
+                    mood TEXT,
                     description TEXT
                 );
             """;
 
-            String createBudgetTable = """
-                CREATE TABLE IF NOT EXISTS budgets (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    category TEXT UNIQUE NOT NULL,
-                    original REAL NOT NULL,
-                    current REAL NOT NULL,
-                    edited INTEGER DEFAULT 0
-                );
-            """;
-
             stmt.execute(createExpensesTable);
-            stmt.execute(createBudgetTable);
+
+            stmt.execute("""
+             CREATE TABLE IF NOT EXISTS budgets (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             username TEXT,
+             month TEXT,
+             category TEXT,
+             amount REAL,
+             edited INTEGER DEFAULT 0,
+             UNIQUE(username, month, category)
+              );
+            """);
 
         } catch (SQLException e) {
             e.printStackTrace();
